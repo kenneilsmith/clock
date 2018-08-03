6bd744416380819a9fc9e8624adae4975991fbde
Damian Smith <damiansmith61@gmail.com>
2018-08-03 13:46:41 -0400

    inital commit

--- script.js
+++ script.js
@@ -1,82 +1,72 @@
-function arr(N) {
-  var numbers = [];
-  for (var i = 0; i < N; i++) {
-    numbers.push(i);
+// 1 - Reverse a String
+
+// Write a JavaScript function that takes in a string and reverses it. A for loop would be handy for this!
+
+function reverseWord(word) {
+  var letterHolder = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    letterHolder += word[i];
   }
-  return numbers;
+  console.log(letterHolder);
 }
 
-// console.log(arr(7));
+reverseWord("Watch me work!");
+reverseWord("!!!!taht ees uoy diD");
+console.log(
+  "-------------------------------------------------------------------------------------------------------------------------------------------------------"
+);
+// 2 - FizzBuzz
 
-document.querySelector("body").style = "background:lightgray";
-document.querySelector("h1").style = "color:white; text-align:center";
-var minuteDeg = 180;
-document.querySelector("#minuteHand").style =
-  "transform: rotate(" + minuteDeg + "deg)";
-setInterval(function() {
-  var date = new Date();
-  var hour = "" + date.getHours();
-  if (hour.length == 1) {
-    hour = "0" + hour;
+// Write a program that console.logs the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five console.log “Buzz”. For numbers which are multiples of both three and five console.log “FizzBuzz”. Hint: Look into the '%' operator
+function oneToOneHundred() {
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
   }
-  if (hour > 12) {
-    hour -= 12;
+}
+oneToOneHundred();
+
+console.log(
+  "-------------------------------------------------------------------------------------------------------------------------------------------------------"
+);
+// 3 - Pig Latin
+
+// Pig Latin is a game of alterations played on the English language. To create the Pig Latin form of an English word the initial consonant sound is transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield "anana-bay"). Check out Wikipedia (Links to an external site.)Links to an external site. for more information on rules - try to get as close as possible.
+
+// Compose a javascript function that takes in a regular sentence or paragraph and returns the Pig Latin equivalent.
+var firstLetter = "";
+var restOfWord = "";
+var pigLatinWord = "";
+
+function pigLatin(word) {
+  if (
+    word[0].toLowerCase() == "a" ||
+    word[0].toLowerCase() == "e" ||
+    word[0].toLowerCase() == "i" ||
+    word[0].toLowerCase() == "o" ||
+    word[0].toLowerCase() == "u"
+  ) {
+    pigLatinWord = word + "ay";
+    console.log(word);
+  } else {
+    for (var i = 1; i < word.length; i++) {
+      restOfWord += word[i];
+    }
+    console.log(word + " test");
+    console.log(restOfWord);
+    firstLetter += word[0] + "ay";
+    pigLatinWord = restOfWord + firstLetter;
   }
-  function am_pm() {
-    if (hour.length == 1) {
-      return "AM";
-    } else return "PM";
-  }
-  var minute = "" + date.getMinutes();
-  if (minute.length == 1) {
-    minute = "0" + minute;
-  }
-  var second = "" + date.getSeconds();
-  if (second.length == 1) {
-    second = "0" + second;
-  }
 
-  document.getElementById("digital").innerText =
-    hour + ":" + minute + ":" + second + " " + am_pm();
-}, 1000);
+  return pigLatinWord;
+}
 
-setInterval(function() {
-  var date = new Date();
-  var second = date.getSeconds();
-  var minute = date.getMinutes();
-  var hour = date.getHours();
-  var secondHand = document.querySelector("#secondHand");
-  var minuteHand = document.querySelector("#minuteHand");
-  var hourHand = document.querySelector("#hourHand");
-
-  var secondStartDeg = second * 6;
-  document.querySelector("#secondHand").style =
-    "transform: rotate(" + secondStartDeg + "deg)";
-
-  // percentage of seconds out of a minute, so at 30s thats 50% * 6deg
-
-  var minuteStartDeg = minute * 6 + secondStartDeg / 60;
-  document.querySelector("#minuteHand").style =
-    "transform: rotate(" + minuteStartDeg + "deg)";
-  if (hour > 12) {
-    hour -= 12;
-  }
-  // console.log(secondStartDeg + "deg second");
-  // console.log(minuteStartDeg + "deg minute");
-
-  var hourStartDeg = hour * 30 + minuteStartDeg / 60;
-  // console.log(hourStartDeg + "deg hour");
-  document.querySelector("#hourHand").style =
-    "transform: rotate(" + hourStartDeg + "deg)";
-}, 1000);
-
-// console.log(image1[0]);
-
-$(document).ready(function() {
-  $(".images").on("click", function() {
-    var src = $(this).attr("src");
-    $("#clock-face").css("background-image", "url(" + src + ")");
-  });
-});
-
-// console.log(getImg.src);
+pigLatin("amian");
+console.log(pigLatinWord);
